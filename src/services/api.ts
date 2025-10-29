a68db181aeb56ec871f8a5ee18f715127f9bd21a
Rafaela <rafaelajung12@gmail.com>
2025-10-29 19:41:55 -0300

    feat: Adicionando vercel.json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,11 @@
 import axios from "axios";
 
-export const api = axios.create({baseURL: "http://localhost:8080"})
+const apiBase =
+  (typeof window !== "undefined" && (window as any).__API_BASE) ||
+  localStorage.getItem("API_BASE") ||
+  process.env.REACT_APP_API_BASE_URL;
+
+export const api = axios.create({
+  baseURL: apiBase,
+  // withCredentials: true, // se usar cookie de sessão
+});
